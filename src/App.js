ba06800446395e56123592bd38b5d9a85418e6de
pnowak <piotr.nowak.25.02.1978@gmail.com>
2019-01-16 19:21:40 +0100

    handleClick and rest

--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { rating: 0 };
+
+    //This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event, target) {
+    const li = Array.from(document.querySelectorAll('li'));
+
+    li.forEach((item, index) => {
+      item.classList.remove('active');
+
+      if (index + 1 <= target) {
+        item.classList.add('active');
+      }
+    });
+
+    this.setState(() => ({
+      rating: target
+    }));
+  }
+
   render() {
     return (
       <div className="rating">
         <ul className="list">
-          <li className="star active">&#9733;</li>
-          <li className="star active">&#9733;</li>
-          <li className="star active">&#9733;</li>
-          <li className="star">&#9734;</li>
-          <li className="star">&#9734;</li>
+          <li className="star" onClick={(event) => this.handleClick(event, 1)}>&#9734;</li>
+          <li className="star" onClick={(event) => this.handleClick(event, 2)}>&#9734;</li>
+          <li className="star" onClick={(event) => this.handleClick(event, 3)}>&#9734;</li>
+          <li className="star" onClick={(event) => this.handleClick(event, 4)}>&#9734;</li>
+          <li className="star" onClick={(event) => this.handleClick(event, 5)}>&#9734;</li>
         </ul>
-        <span>3 of 5</span>
+        <span>{this.state.rating} of 5</span>
       </div>
     );
   }
